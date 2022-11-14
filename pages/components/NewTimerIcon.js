dc491590f97786cd7c0b3d5b8cf7ea6832ab5571
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-14 18:53:57 -0500

    add icon and animation

--- pages/components/NewTimerIcon.js
+++ pages/components/NewTimerIcon.js
@@ -1,12 +1,39 @@
-import addIcon  from './add.png';
 
 
 function NewTimerIcon() {
   return (
-    <div>
-     <img src={addIcon} alt="add icon" />
+    <div className="add-container">
+      <img src="/add.png" alt="add icon" />
+
       <style jsx>{`
+       .add-container {
+          width: 80px;
+          height: 80px;
+          position : absolute;
+          bottom: 20px;
+          right: 20px;
+        }
+        img {
+          width: 100%;
+          height: 100%;
+          filter : invert(1);
+          cursor: pointer;
+        }
+
+        img:hover {
+          animation : rotate 1s ease ; 
+        }
         
+        @keyframes rotate {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+       
+
       `}</style>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import NewTimerIcon from "./components/NewTimerIcon";
 
 export default function Home() {
-  return <div>
-    <NewTimerIcon/>
-  </div>;
+  return (
+    <NewTimerIcon />
+  )
+  ;
 }
