cd4d66c1bd9488054c496dc01fe0675f8a13fe91
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-14 17:56:10 -0500

    background gradient and icon

---
+++ pages/components/NewTimerIcon.js
@@ -0,0 +1,15 @@
+import addIcon  from './add.png';
+
+
+function NewTimerIcon() {
+  return (
+    <div>
+     <img src={addIcon} alt="add icon" />
+      <style jsx>{`
+        
+      `}</style>
+    </div>
+  );
+}
+
+export default NewTimerIcon;
