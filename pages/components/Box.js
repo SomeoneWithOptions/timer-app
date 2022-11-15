64686a9bc94f5fed53c8c7a9bd7eb26c722b6506
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-15 17:05:16 -0500

    fix body height

--- pages/components/Box.js
+++ pages/components/Box.js
@@ -1,14 +1,14 @@
 function Box({ children }) {
   return (
     <div className="box">
-      <p>{children}</p>
+      <span>{children}</span>
       <style jsx>{`
         .box {
           border: solid 1px var(--neutral-200);
           margin: 0.5rem;
           background-color: var(--neutral-200);
           border-radius: 1rem;
-          height: 250px;
+          height: 200px;
           padding: 1rem;
           box-shadow: var(--shadow);
           display: flex;
