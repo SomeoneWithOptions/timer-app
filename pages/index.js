b3f11ec2b1103d46fe1d0ca424a9f3ac7dba48f0
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-17 15:40:53 -0500

    spacing fixes

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,11 @@ import NewTimerIcon from "./components/NewTimerIcon";
 import AddTimerMenu from "./components/AddTimerMenu";
 import MinutesBox from "./components/MinutesBox";
 import SecondsBox from "./components/SecondsBox";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Head from "next/head";
 
 export default function Home() {
   const [menuIsVisible, setMenuIsVisible] = useState(false);
-  
 
   return (
     <>
